7762fa89c02bb694e725a7559926957d24f5adbe
haoala <haoala@gmail.com>
2015-12-29 15:38:53 -0600

    Primitive acceleration

--- Assets/CameraMovement.cs
+++ Assets/CameraMovement.cs
@@ -4,10 +4,12 @@ using System.Collections;
 public class CameraMovement : MonoBehaviour {
 
     // Camera movement speed
-    public float movementSpeed = 0.5f;
-    public float horizontalRotationSpeed = 2f;
-    public float verticalRotationSpeed = 2f;
+    public float movementSpeed = 0.25f;
+    public float horizontalRotationSpeed = 5f;
+    public float verticalRotationSpeed = 5f;
 
+    private float defaultMovementSpeed = 0.25f;
+    private float maxMovementSpeed = 5f;
     private float yaw = 0f;
     private float pitch = 0f;
 
@@ -23,7 +25,7 @@ public class CameraMovement : MonoBehaviour {
         front.y = 0;
         front.Normalize();
 
-        Debug.Log(front);
+        //Debug.Log(front);
 
 
         yaw += horizontalRotationSpeed * Input.GetAxis("Mouse X");
@@ -35,19 +37,25 @@ public class CameraMovement : MonoBehaviour {
         //forward/back motion
         if (Input.GetKey(KeyCode.W))
         {
+            movementSpeed += 0.01f;
+            movementSpeed = Mathf.Min(maxMovementSpeed, movementSpeed);
             transform.Translate(movementSpeed * front, Space.World);
         }
-        else if (Input.GetKey(KeyCode.S))
-        {
-            transform.Translate(-movementSpeed * front, Space.World);
+        else {
+            movementSpeed = defaultMovementSpeed;
+            if (Input.GetKey(KeyCode.S)){
+                transform.Translate(-movementSpeed * front, Space.World);
+            }
         }
 
         if (Input.GetKey(KeyCode.A))
         {
+            movementSpeed = defaultMovementSpeed;
             transform.Translate(movementSpeed * left, Space.World);
         }
         else if (Input.GetKey(KeyCode.D))
         {
+            movementSpeed = defaultMovementSpeed;
             transform.Translate(-movementSpeed * left, Space.World);
         }
 	}
